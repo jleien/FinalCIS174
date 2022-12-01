49af0d1540020b260840cf9b955711f63964ca3a
Alex <112102138+34ykri@users.noreply.github.com>
2022-12-01 09:50:37 -0600

    first push

---
+++ FinalCIS174/Models/DNDContext.cs
@@ -0,0 +1,18 @@
+﻿using FinalCIS174.Models.UserLogin;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalCIS174.Models
+{
+    public class DNDContext : IdentityDbContext<User>
+    {
+        public DNDContext(DbContextOptions<DNDContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
