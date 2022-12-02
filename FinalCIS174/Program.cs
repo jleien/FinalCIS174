dee1954c175af329adebb12047905bbfb0ba7726
Alex <112102138+34ykri@users.noreply.github.com>
2022-12-02 10:05:44 -0600

    database updated

--- FinalCIS174/Program.cs
+++ FinalCIS174/Program.cs
@@ -1,8 +1,22 @@
+using FinalCIS174.Models;
+using FinalCIS174.Models.UserLogin;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+//Session state/cookies
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
+builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+//DBContext
+builder.Services.AddDbContext<DNDContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DNDContext")));
+builder.Services.AddIdentity<User, IdentityRole>(options => {
+    //Password must be 8 characters long with at least one lowercase letter,
+    //one uppercase letter, one number, and one special character
+}).AddEntityFrameworkStores<DNDContext>().AddDefaultTokenProviders();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,8 +31,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
