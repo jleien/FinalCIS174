730ac7e8b526e740a7c017f0e9fbdf1487ae3332
DESKTOP-OQ0O92M\kajak <jleiendecker@dmacc.edu>
2022-12-03 21:29:08 -0600

    Edit Method started, can't seem to get it to work.

--- FinalCIS174/Controllers/PlayerController.cs
+++ FinalCIS174/Controllers/PlayerController.cs
@@ -61,7 +61,7 @@ namespace FinalCIS174.Controllers
                 Player = context.Players.Include(c => c.Race).Include(c => c.Class).FirstOrDefault(c => c.PlayerID == id),
                 ActiveRace = session.GetActiveRace(),
                 ActiveClass = session.GetActiveClass(),
-        };
+            };
             return View(model);
         }
 
@@ -116,6 +116,37 @@ namespace FinalCIS174.Controllers
             }
 
         }
+
+        public ActionResult EditPlayer(string? PlayerID)
+        {
+            var data = context.Players.Where(x => x.PlayerID == PlayerID).FirstOrDefault();
+            return View(data);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPlayer(string PlayerID, Player model)
+        {
+            var data = context.Players.Where(x => x.PlayerID == PlayerID).FirstOrDefault();
+
+            if (data != null)
+            {
+                data.PlayerID = model.PlayerID;
+                data.Name = model.Name;
+                data.Level = model.Level;
+                data.ClassID = model.ClassID;
+                data.RaceID = model.RaceID;
+                context.SaveChanges();
+                return RedirectToAction("Details");
+            }
+            else
+                return View();
+        }
+
+
+
+
         [NonAction]
         public string GetSlug(string name)
         {
@@ -123,3 +154,4 @@ namespace FinalCIS174.Controllers
         }
     }
 }
+
