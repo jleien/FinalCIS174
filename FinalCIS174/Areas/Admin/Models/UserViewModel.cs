68df2ab874296f5290eba44426ea22606768dfe3
Jake Leiendecker <97557098+jleien@users.noreply.github.com>
2022-12-05 15:37:57 -0600

    Revert "Fixed spacing issues with Hello User message in layout"

---
+++ FinalCIS174/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FinalCIS174.Models.UserLogin;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinalCIS174.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+    }
+}
